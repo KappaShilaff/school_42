b23cb1d010c76cef8aa9762b35ae03b07fd3def7
KappaShilaff <KappaShilaff@users.noreply.github.com>
2019-10-15 01:48:18 +0300

    rb in Makefile

---
+++ libft/includes/librb.h
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lcassaun <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/05 16:55:12 by lcassaun          #+#    #+#             */
+/*   Updated: 2019/09/24 21:28:27 by lcassaun         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBRB_H
+# define LIBRB_H
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+enum	rb_color
+{
+	BLACK,
+	RED
+};
+
+typedef struct			s_node
+{
+	void				*d;
+	enum rb_color		c;
+	struct s_node		*l;
+	struct s_node		*r;
+	struct s_node		*p;
+}						t_node;
+
+t_node					*ft_create_node(void *data);
+t_node					*ft_insert_node(t_node *root, t_node *t);
+t_node					*ft_grandp_node(t_node *t);
+t_node					*ft_uncle_node(t_node *t);
+void					ft_rotate_left(t_node *t);
+void					ft_rotate_right(t_node *t);
+void					ft_insert_case1(t_node *t);
+void					ft_insert_case2(t_node *t);
+void					ft_insert_case3(t_node *t);
+void					ft_insert_case4(t_node *t);
+void					ft_insert_case5(t_node *t);
+
+#endif
---
+++ libft/srcs/ft_rotate.c
@@ -0,0 +1,47 @@
+#include "librb.h"
+
+void	ft_rotate_left(t_node *t)
+{
+	t_node *pivot;
+
+		if (!(t->r))
+			return ;
+		pivot = t->r;
+		pivot->p = t->p;
+		if (t->p != NULL) 
+		{
+			if (t->p->l == t)
+				t->p->l = pivot;
+			else
+				t->p->r = pivot;
+		}		
+		t->r = pivot->l;
+		if (pivot->l != NULL)
+			pivot->l->p = t;
+
+		t->p = pivot;
+		pivot->l = t;
+}
+
+void	ft_rotate_right(t_node *t)
+{
+	t_node *pivot = t->l;
+
+	if (!(t->l))
+		return ;
+	pivot = t->l;
+	pivot->p = t->p; 
+	if (t->p != NULL) 
+	{
+		if (t->p->l == t)
+			t->p->l = pivot;
+		else
+			t->p->r = pivot;
+	}		
+	t->l = pivot->r;
+	if (pivot->r != NULL)
+		pivot->r->p = t;
+
+	t->p = pivot;
+	pivot->r = t;
+}
