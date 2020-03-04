aa694b5e0c007de8f87b424758484531f88dfb6e
KappaShilaff <KappaShilaff@users.noreply.github.com>
2020-03-04 03:46:31 +0300

    start d flag

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -10,22 +10,17 @@ void    ft_sstar(va_list arg, char *str)
     char    *out;
     char    minus;
     char    plus;
-    char    zero;
     char    space;
 
-//    printf("\n%s\n", str);
     out = str;
     minus = '+';
     plus = '-';
-    zero = '1';
     space = '\0';
     while (*str != '*' && (*str == ' ' || *str == '0' || *str == '-' || *str == '+')) {
         if (*str == '-')
             minus = '-';
         if (*str == '+')
             plus = '+';
-        if (*str == '0')
-            zero = '0';
         if (*str == ' ')
             space = ' ';
         *str++;
@@ -62,6 +57,87 @@ void    ft_sstar(va_list arg, char *str)
         while (*out != 's')
             write(1, &(*out++), 1);
         write(1, "s", 1);
+    }
+}
+
+int     ft_dflag(char *str, va_list arg)
+{
+    char    minus;
+    int     i;
+    char    *out;
+    int     k;
+    char    star;
+    char    *temp;
+    char    ll = '0';
+    char    l = '0';
+    char    h = '0';
+    char    L = '0';
+    char    hh = '0';
+    char    j = '0';
+    char    z = '0';
+
+    temp = str;
+    star = '0';
+    i = 0;
+    minus = '+';
+    while (*str == ' ' || *str == '+' || *str == '-' || *str == '*' || (*str >= '0' && *str <= '9') || *str == 'l' || *str == 'z' || *str == 'h' || *str == 'L' || *str == 'j')
+    {
+        if (*str == '-')
+            minus = '-';
+        if (*str == '*')
+            star = '1';
+        if (*str == 'L')
+            L = 'L';
+        if (*str == 'h') {
+            if (*(str + 1) == 'l') {
+                hh = 'h';
+                *str++;
+            } else
+                h = 'h';
+        }
+        if (*str == 'l') {
+            if (*(str + 1) == 'l') {
+                ll = 'l';
+                *str++;
+            } else
+                l = 'l';
+        }
+        if (*str == 'j')
+            j = 'j';
+        if (*str == 'z')
+            z = 'z';
+        *str++;
+    }
+    if (*str != 'd')
+        return (0);
+    if (star == '1')
+    {
+        i = va_arg(arg, int);
+        return (1);
+    }
+    else {
+        while (*temp == '-' || *temp == '+' || *temp == ' ' || *temp == '0')
+            *temp++;
+        i = ft_atoi(temp);
+    }
+//  napisat' CASE l L ll h hh z j dlya va_arg a ewe napisat' strlen dlya chisel chtob virovnyalka rabotala ny i kone4no putnbr dlya vseh types
+        out = va_arg(arg, char *);
+        if (i != 0) {
+            k = i - ft_strlen(out);
+            if (minus == '+') {
+                while (k-- > 0)
+                    write(1, " ", 1);
+            }
+            while (*out)
+                write(1, &(*out++), 1);
+            while (k-- > 0)
+                write(1, " ", 1);
+            return (1);
+        } else {
+            while (*out)
+                write(1, &(*out++), 1);
+            return (1);
+        }
     }
 }
 
@@ -124,11 +200,13 @@ char    *ft_flag(char *str, va_list arg) {
 
     tmp = str;
     n = 0;
-    while (str[n] && (str[n] != 's'))
+    while (str[n] && ((str[n] != 's') || str[n] != 'd'))
         n++;
     if (!str[n])
         return (tmp + 1);
-    if (str[n] == 's' && !ft_sflag(tmp, arg))
+    if (str[n] == 's'  && !ft_sflag(tmp, arg))
+        return (tmp + 1);
+    if (str[n] == 'd'  && !ft_dflag(tmp, arg))
         return (tmp + 1);
     return (tmp + n + 1);
 }
--- ft_printf/main.c
+++ ft_printf/main.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 #include <unistd.h>
-
-int		ft_printf(const char *format, ...);
+#include "stdlib.h"
+//int		ft_printf(const char *format, ...);
 
 int		main(void)
 {
-    char    *str;
-    str = "%321-   0 0 0     0    0       -  123123  *s%s";
-    printf("MOE\n");
-    ft_printf(str,"lol", " kek");
-    printf("\nORIG\n");
-    printf(str, "lol", " kek");
+
+//    char    *str;
+//    str = "%321-   0 0 0     0    0       -  123123  *s%s";
+//    printf("MOE\n");
+//    ft_printf(str, "lol", " kek");
+
+
+//    printf("\nORIG\n");
+//    printf(str, "lol", " kek");
+    printf("%+**ld", 50, 1);
+
     return (0);
 }
