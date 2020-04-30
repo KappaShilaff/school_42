fbbd5f774c9335b62bfe46fad2a5dcce51a87003
KappaShilaff <ckiper@yandex.ru>
2020-04-30 12:33:20 +0300

    upd mantissa and exponent

--- ft_printf/main.c
+++ ft_printf/main.c
@@ -10,8 +10,8 @@ int				main(void)
 //    int     ded = 1;
 //    i = ft_printf("%#.3o", 1);
 //    printf("\n");
-    ft_printf("%f", (double)6);
-    printf("%d", (unsigned long int)(double)-1 >> 63 & 1u);
+    ft_printf("%f", (double)5);
+//    printf("hehe %d\n", (((unsigned long int)(double)-100 >> 62) & 1u));
 //    printf("\n%d\n%d", i, k);
     return (0);
 }
--- ft_printf/srcs/fflag.c
+++ ft_printf/srcs/fflag.c
@@ -7,61 +7,44 @@
 
 void    ft_fnumber(struct s_part *part)
 {
-    unsigned long int    e, k, l, kek, mt;
-    unsigned long int     i[52];
-
+    int    e, k, l, mt, i[11];
+    unsigned long int       kek;
     char    *mant;
 
-    mant = malloc(55);
-    mant[54] = '\0';
-    mt = 53;
     e = -1023;
     k = 1;
     i[0] = 1;
-    while (k < 52) {
-        i[k] = i[k - 1] * 2;
-        k++;
-    }
+    while (k++ < 11)
+        i[k - 1] = i[k - 2] * 2;
     part->dnum = va_arg(*part->arg, double);
     ft_memcpy(&kek, &part->dnum, 8);
     k = 0;
     l = 0;
-    while (k < 52)
-    {
-        if (((kek >> l) & 1u) == 1) {
-            write(1, "1", 1);
-            mant[mt] = '1';
-        } else {
-            write(1, "0", 1);
-            mant[mt] = '0';
-        }
-        mt--;
-        l++;
-        k++;
-    }
+    while (k < 52 && k != 100)
+        if (((kek >> l++) & 1u) == 1)
+            k = 100;
+        else
+            k++;
+    l -= 1;
+    mt = 53 - l;
+    mant = ft_chmalloc_zend(mt + 1);
+    k = l;
+    while (k++ < 52)
+        if (((kek >> l++) & 1u) == 1)
+            mant[mt--] = '1';
+        else
+            mant[mt--] = '0';
     mant[1] = '.';
     mant[0] = '1';
     k = 0;
-    while (k < 11)
-    {
-        if (((kek >> l) & 1u) == 1) {
-            e += i[k];
-            write(1, "1", 1);
-        } else {
-            write(1, "0", 1);
-        }
-        l++;
-        k++;
-    }
-    if (((kek >> l & 1u)) == 1) {
+    while (k++ < 11)
+        if (((kek >> l++) & 1u) == 1)
+            e += i[k - 1];
+    if (((kek >> l & 1u)) == 1)
         part->negative = 1;
-        write(1, "1", 1);
-    }
-    else {
-        write(1, "0", 1);
-    }
-    printf("\nmantissa %s", mant);
-    printf("\ne %llu\n", e);
+    if (e == -1023)
+        mant[0] = '0';
+    printf("mantissa %s\nexponent %d\nnegative %d", mant, e, part->negative);
 }
 
 int     ft_fflag(char *str, struct s_part *part)
