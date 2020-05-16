312ceb0dfd524a2aa63b5d4e7c110c90bc29c9a5
KappaShilaff <ckiper@yandex.ru>
2020-05-16 19:25:32 +0300

    ft_chround fixed. 4.9 -> 5 | 0.5 -> 1 | 14.44 -> 14.4

--- ft_printf/srcs/libft/srcs/ft_chround.c
+++ ft_printf/srcs/libft/srcs/ft_chround.c
@@ -25,13 +25,38 @@ static void norme1(int i[1], int k[1], int n[1], char **str)
     while ((*str)[i[0]] && (*str)[i[0]] != '.')
         i[0]++;
     if (i[0] + n[0] + 1 < (int)ft_strlen(*str))
-        if ((*str)[i[0] + n[0] + 1] >= '4'  && (*str)[i[0] + n[0] + 1] <= '9')
+        if ((*str)[i[0] + n[0] + 1] >= '5'  && (*str)[i[0] + n[0] + 1] <= '9')
         {
             buf = zeronum(n[0]);
             *str = ft_chsum(*str, buf);
             free(buf);
             k[0] = 1;
         }
+}
+
+static char    *norme2(char **str, int i, int k) {
+    char *buf;
+    char *buf2;
+
+    while ((*str)[i] == '0')
+        i++;
+    if ((*str)[i] == '.') {
+        if ((*str)[i + 1] && (*str)[i + 1] >= '5' && (*str)[i + 1] <= '9')
+            buf = ft_strdup("1");
+        else
+            buf = ft_strdup("0");
+    } else {
+        while ((*str)[i + k] != '.')
+            k++;
+        buf = ft_chmalloc_zend(k);
+        if ((*str)[i + k + 1] && (*str)[i + k + 1] >= '5' && (*str)[i + k + 1] <= '9')
+            buf2 = ft_chsum(*str, "1.0");
+        else
+            buf2 = ft_strdup(*str);
+        ft_strncpy(buf, buf2, k);
+        free(buf2);
+    }
+    return (buf);
 }
 
 char    *ft_chround(char *str, int n)
@@ -41,6 +66,8 @@ char    *ft_chround(char *str, int n)
     int     size;
     int     k;
 
+    if (n == 0)
+        return (norme2(&str, 0, 0));
     norme1(&i, &k, &n, &str);
     res = ft_chmalloc_zend(i + n + 1);
     size = i + n;
@@ -50,10 +77,9 @@ char    *ft_chround(char *str, int n)
         res[i] = str[i];
         i++;
     }
-    while (size-- >= -1)
+    while (size-- >= -1) {
         res[i++] = '0';
-    if (n == 0)
-        res[i - 1] = '\0';
+    }
     if (k == 1)
         free(str);
     return (res);
