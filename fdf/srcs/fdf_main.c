7eda9407f5434d9b1711ef907bec2fe0f82d53cf
KappaShilaff <ckiper@yandex.ru>
2020-06-26 14:15:37 +0300

    test grafen

--- fdf/srcs/fdf_main.c
+++ fdf/srcs/fdf_main.c
@@ -6,11 +6,12 @@
 /*   By: lcassaun <lcassaun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/25 16:36:37 by lcassaun          #+#    #+#             */
-/*   Updated: 2020/06/25 23:25:19 by lcassaun         ###   ########.fr       */
+/*   Updated: 2020/06/26 14:11:02 by lcassaun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fdf.h"
+#include "mlx.h"
 #include <stdio.h>
 
 int 	free_mlx(t_mlx *mlx)
@@ -28,6 +29,8 @@ int main(int ac, char **argv)
 	t_mlx	*mlx;
 	int 	temp_x;
 	int 	temp_y;
+	void 	*mlx_ptr;
+	void 	*win_ptr;
 
 	mlx = map_parsing(argv);
 	if (mlx == NULL)
@@ -48,5 +51,10 @@ int main(int ac, char **argv)
 	mlx->x_max = temp_x;
 	mlx->y_max = temp_y;
 	free_mlx(mlx);
+	mlx_ptr = mlx_init();
+	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "fdf");
+	mlx_pixel_put(mlx_ptr, win_ptr, 250, 250, 0xffffff);
+	mlx_string_put(mlx_ptr, win_ptr, 100, 100, "lol");
+	mlx_loop(mlx_ptr);
 	return (0);
 }
