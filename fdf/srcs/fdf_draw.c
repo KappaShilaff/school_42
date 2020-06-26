61e3925bcf3136448ff6508e4731dd3a2b2cb5b9
KappaShilaff <ckiper@yandex.ru>
2020-06-26 15:21:33 +0300

    another one

--- fdf/srcs/fdf_draw.c
+++ fdf/srcs/fdf_draw.c
@@ -6,7 +6,7 @@
 /*   By: lcassaun <lcassaun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/26 14:22:50 by lcassaun          #+#    #+#             */
-/*   Updated: 2020/06/26 15:16:58 by lcassaun         ###   ########.fr       */
+/*   Updated: 2020/06/26 15:20:24 by lcassaun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,15 @@ int 	draw_line(t_mlx *mlx)
 	float 	y_step;
 	int 	max;
 
-	x_step = mlx->x1 - mlx->x2;
-	y_step = mlx->y1 - mlx->y2;
+	x_step = mlx->x2 - mlx->x1;
+	y_step = mlx->y2 - mlx->y1;
 	if (x_step > y_step)
 		max = (int)x_step;
 	else
 		max = (int)y_step;
 	x_step /= (float)max;
 	y_step /= (float)max;
-	while ((int)(mlx->x1 - mlx->x2) || (int)(mlx->y2 - mlx->y1))
+	while ((int)(mlx->x2 - mlx->x1) || (int)(mlx->y2 - mlx->y1))
 	{
 		mlx_pixel_put(mlx->mlx_ptr, mlx->win_ptr, (int)mlx->x1, (int)mlx->y1, 0xffffff);
 		mlx->x1 += x_step;
