957a5e3e8805ea9bf77550cb9aa10e1b76de29e7
KappaShilaff <ckiper@yandex.ru>
2020-06-26 14:44:08 +0300

    draw line test fix

--- fdf/includes/fdf.h
+++ fdf/includes/fdf.h
@@ -6,13 +6,17 @@
 /*   By: lcassaun <lcassaun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/25 16:37:26 by lcassaun          #+#    #+#             */
-/*   Updated: 2020/06/25 18:59:40 by lcassaun         ###   ########.fr       */
+/*   Updated: 2020/06/26 14:32:27 by lcassaun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FDF_FDF_H
 # define FDF_FDF_H
+# define WIDTH		1600
+# define HEIGHT		900
+# define MENU		250
 # include "../srcs/libft/includes/libft.h"
+# include "mlx.h"
 
 typedef struct		s_mlx
 {
@@ -26,5 +30,6 @@ typedef struct		s_mlx
 
 t_mlx 	*map_parsing(char **av);
 int 	fdf_valid_malloc(t_mlx *mlx, char **av);
+int 	draw(void *mlx_ptr, void *win_ptr, t_mlx *mlx);
 
 #endif
