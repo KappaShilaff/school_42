e5be512910fda3dd605e686101aefed173fb0044
KappaShilaff <ckiper@yandex.ru>
2020-06-26 14:42:48 +0300

    draw line test

---
+++ fdf/srcs/fdf_draw.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf_draw.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lcassaun <lcassaun@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/26 14:22:50 by lcassaun          #+#    #+#             */
+/*   Updated: 2020/06/26 14:32:27 by lcassaun         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/fdf.h"
+
+int 	draw(void *mlx_ptr, void *win_ptr, t_mlx *mlx)
+{
+	float 	x;
+	float 	x1;
+	float 	y;
+	float 	y1;
+	float 	max;
+	float 	temp_max;
+	float 	step_x;
+	float 	step_y;
+
+	x = 10;
+	x1 = 50;
+	y = 20;
+	y2 = 100;
+	max = ft_abs((long)(x - x1));
+	max_temp = ft_abs((long)(y - y1));
+	if (max_temp > max)
+		max = max_temp;
+	step_x = (x1 - x) / max;
+	step_y = (y1 - y) / max;
+	while((int)(x1 - x) > 0 || (int)(y1 - y) > 0)
+	{
+		mlx_pixel_put(mlx_ptr, win_ptr, (int)x, (int)y, oxffffff);
+		x += step_x;
+		y += step_y;
+	}
+}
--- fdf/srcs/fdf_main.c
+++ fdf/srcs/fdf_main.c
@@ -6,12 +6,11 @@
 /*   By: lcassaun <lcassaun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/25 16:36:37 by lcassaun          #+#    #+#             */
-/*   Updated: 2020/06/26 14:15:13 by lcassaun         ###   ########.fr       */
+/*   Updated: 2020/06/26 14:32:27 by lcassaun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fdf.h"
-#include "mlx.h"
 #include <stdio.h>
 
 int 	free_mlx(t_mlx *mlx)
@@ -38,23 +37,24 @@ int main(int ac, char **argv)
 		ft_putstr("invalid otstrelen\n");
 		return (0);
 	}
-	printf("x = %d\ny = %d\n", mlx->x_max, mlx->y_max);
-	temp_x = mlx->x_max;
-	temp_y = mlx->y_max;
-	while ((mlx->y_max)-- != 1)
-	{
-		mlx->x_max = temp_x;
-		while ((mlx->x_max-- != 1))
-			printf("%3.0f ", (mlx->pxl)[temp_y - mlx->y_max][temp_x - mlx->x_max]);
-		printf("\n");
-	}
-	mlx->x_max = temp_x;
-	mlx->y_max = temp_y;
-	free_mlx(mlx);
 	mlx_ptr = mlx_init();
 	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "fdf");
-	mlx_pixel_put(mlx_ptr, win_ptr, 250, 250, 0xffffff);
-	mlx_string_put(mlx_ptr, win_ptr, 100, 100, 0xffffff, "lol");
+	draw(mlx_ptr, win_ptr, mlx);
 	mlx_loop(mlx_ptr);
+//	printf("x = %d\ny = %d\n", mlx->x_max, mlx->y_max);
+//	temp_x = mlx->x_max;
+//	temp_y = mlx->y_max;
+//	while ((mlx->y_max)-- != 1)
+//	{
+//		mlx->x_max = temp_x;
+//		while ((mlx->x_max-- != 1))
+//			printf("%3.0f ", (mlx->pxl)[temp_y - mlx->y_max][temp_x - mlx->x_max]);
+//		printf("\n");
+//	}
+//	mlx->x_max = temp_x;
+//	mlx->y_max = temp_y;
+//	free_mlx(mlx);
+//	mlx_pixel_put(mlx_ptr, win_ptr, 250, 250, 0xffffff);
+//	mlx_string_put(mlx_ptr, win_ptr, 100, 100, 0xffffff, "lol");
 	return (0);
 }
