ff57f97079479621c9b18b12bdc181ee24b2c5c4
KappaShilaff <ckiper@yandex.ru>
2020-06-28 01:17:33 +0300

    another one

--- fdf/srcs/fdf_draw.c
+++ fdf/srcs/fdf_draw.c
@@ -6,13 +6,13 @@
 /*   By: lcassaun <lcassaun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/26 14:22:50 by lcassaun          #+#    #+#             */
-/*   Updated: 2020/06/28 01:03:19 by lcassaun         ###   ########.fr       */
+/*   Updated: 2020/06/28 01:17:01 by lcassaun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fdf.h"
 
-int 	isometric(t_mlx *mlx, float z1, float z2)
+int			isometric(t_mlx *mlx, float z1, float z2)
 {
 	mlx->x1 = (float)((mlx->x1 - mlx->y1) * cos(0.8));
 	mlx->y1 = (float)((mlx->x1 + mlx->y1) * sin(mlx->x) - z1);
@@ -21,7 +21,7 @@ int 	isometric(t_mlx *mlx, float z1, float z2)
 	return (0);
 }
 
-void	mlx_fill(t_mlx *mlx, float *z1, float *z2)
+void		mlx_fill(t_mlx *mlx, float *z1, float *z2)
 {
 	*z1 = (mlx->pxl)[(int)mlx->y1][(int)mlx->x1];
 	*z2 = (mlx->pxl)[(int)mlx->y2][(int)mlx->x2];
@@ -39,13 +39,13 @@ void	mlx_fill(t_mlx *mlx, float *z1, float *z2)
 	mlx->y2 += (float)mlx->shift_y;
 }
 
-int 	draw_line(t_mlx *mlx)
+int			draw_line(t_mlx *mlx)
 {
-	float 	x_step;
-	float 	y_step;
+	float	x_step;
+	float	y_step;
 	float	z1;
-	float 	z2;
-	int 	max;
+	float	z2;
+	int		max;
 
 	mlx_fill(mlx, &z1, &z2);
 	x_step = mlx->x2 - mlx->x1;
@@ -58,15 +58,17 @@ int 	draw_line(t_mlx *mlx)
 	y_step /= (float)max;
 	while ((int)(mlx->x1 - mlx->x2) || (int)(mlx->y1 - mlx->y2))
 	{
-		if (mlx->x1 > 0 && mlx->y1 > 0 && ((int)mlx->x1 <= WIDTH) && (int)mlx->y1 <= HEIGHT)
-			mlx_pixel_put(mlx->mlx_ptr, mlx->win_ptr, (int)mlx->x1, (int)mlx->y1, mlx->color);
+		if (mlx->x1 > 0 && mlx->y1 > 0 && ((int)mlx->x1 <= WIDTH) &&
+		(int)mlx->y1 <= HEIGHT)
+			mlx_pixel_put(mlx->mlx_ptr, mlx->win_ptr, (int)mlx->x1,
+					(int)mlx->y1, mlx->color);
 		mlx->x1 += x_step;
 		mlx->y1 += y_step;
 	}
 	return (0);
 }
 
-void 	mlx_xy(t_mlx *mlx, int x, int y, int i_switch)
+void		mlx_xy(t_mlx *mlx, int x, int y, int i_switch)
 {
 	if (i_switch == 1)
 	{
@@ -84,12 +86,12 @@ void 	mlx_xy(t_mlx *mlx, int x, int y, int i_switch)
 	}
 }
 
-int 	draw(t_mlx *mlx, int x, int y)
+int			draw(t_mlx *mlx, int x, int y)
 {
 	while (y < mlx->y_max)
 	{
 		x = 0;
-		while(x < mlx->x_max)
+		while (x < mlx->x_max)
 		{
 			if (x < mlx->x_max - 1)
 			{
