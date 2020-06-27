dc0d831323db316ed801fb3dd8bab0928c7b1151
KappaShilaff <ckiper@yandex.ru>
2020-06-28 02:49:59 +0300

    another one

--- fdf/srcs/fdf_color.c
+++ fdf/srcs/fdf_color.c
@@ -6,7 +6,7 @@
 /*   By: lcassaun <lcassaun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/28 02:17:49 by lcassaun          #+#    #+#             */
-/*   Updated: 2020/06/28 02:47:24 by lcassaun         ###   ########.fr       */
+/*   Updated: 2020/06/28 02:48:02 by lcassaun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,5 +36,8 @@ void 	fdf_color(t_mlx *mlx, float z1, float z2)
 		z = (int)z1;
 	else
 		z = (int)z2;
-	mlx->color = clr - (257 * z * 15);
+	if (mlx->clr_switch == 4)
+		mlx->color = clr + (257 * z * 15);
+	else
+		mlx->color = clr - (257 * z * 15);
 }
