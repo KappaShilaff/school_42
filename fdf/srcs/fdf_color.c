bdc5c42e0c15102173628635cac8be2b90b1c1f8
KappaShilaff <ckiper@yandex.ru>
2020-06-28 03:02:20 +0300

    another one

--- fdf/srcs/fdf_color.c
+++ fdf/srcs/fdf_color.c
@@ -6,13 +6,13 @@
 /*   By: lcassaun <lcassaun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/28 02:17:49 by lcassaun          #+#    #+#             */
-/*   Updated: 2020/06/28 02:56:46 by lcassaun         ###   ########.fr       */
+/*   Updated: 2020/06/28 03:02:05 by lcassaun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fdf.h"
 
-void 	fdf_color_switch(t_mlx *mlx, int *clr)
+void	fdf_color_switch(t_mlx *mlx, int *clr)
 {
 	if (mlx->clr_switch == 0)
 		*clr = WHITE;
@@ -26,10 +26,10 @@ void 	fdf_color_switch(t_mlx *mlx, int *clr)
 		*clr = GREEN;
 }
 
-void 	fdf_color(t_mlx *mlx, float z1, float z2)
+void	fdf_color(t_mlx *mlx, float z1, float z2)
 {
-	int 	z;
-	int 	clr;
+	int		z;
+	int		clr;
 
 	fdf_color_switch(mlx, &clr);
 	if (z1 > z2)
@@ -38,9 +38,8 @@ void 	fdf_color(t_mlx *mlx, float z1, float z2)
 		z = (int)z2;
 	if (mlx->clr_switch == 4)
 		mlx->color = clr - (z * 3267);
+	else if (mlx->clr_switch == 2)
+		mlx->color = clr + (257 * z * 15);
 	else
-		if (mlx->clr_switch == 2)
-			mlx->color = clr + (257 * z * 15);
-		else
-			mlx->color = clr - (257 * z * 15);
+		mlx->color = clr - (257 * z * 15);
 }
