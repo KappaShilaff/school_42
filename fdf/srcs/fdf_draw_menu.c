8a01f26dcbde25bd6f0d8997a898a8258720f93b
KappaShilaff <ckiper@yandex.ru>
2020-06-30 15:31:48 +0300

    another one

--- fdf/srcs/fdf_draw_menu.c
+++ fdf/srcs/fdf_draw_menu.c
@@ -6,7 +6,7 @@
 /*   By: lcassaun <lcassaun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/30 15:11:17 by lcassaun          #+#    #+#             */
-/*   Updated: 2020/06/30 15:16:38 by lcassaun         ###   ########.fr       */
+/*   Updated: 2020/06/30 15:30:55 by lcassaun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,15 @@
 void	drow_menu(t_mlx *mlx)
 {
 	mlx_string_put(mlx->mlx_ptr, mlx->win_ptr,
-			30, 30, 0xffffff, "key_shift        - w a s d\n"
-	"key_zoom         - up down\n"
-	"key_zoom_z       - left right\n"
-	"key_rotation     - q e\n"
-	"key_change_color - f\n"
-	"exit             - esc");
+		30, 80, 0xffffff, "key_shift        - w a s d");
+	mlx_string_put(mlx->mlx_ptr, mlx->win_ptr,
+		30, 130, 0xffffff, "key_zoom         - up down");
+	mlx_string_put(mlx->mlx_ptr, mlx->win_ptr,
+		30, 180, 0xffffff, "key_zoom_z       - left right");
+	mlx_string_put(mlx->mlx_ptr, mlx->win_ptr,
+		30, 230, 0xffffff, "key_rotation     - q e");
+	mlx_string_put(mlx->mlx_ptr, mlx->win_ptr,
+		30, 280, 0xffffff, "key_change_color - f");
+	mlx_string_put(mlx->mlx_ptr, mlx->win_ptr,
+		30, 320, 0xffffff, "exit             - esc");
 }
