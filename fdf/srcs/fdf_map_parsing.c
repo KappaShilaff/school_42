b02a101cd7c797c48c28e36e61c818901132e4bb
KappaShilaff <ckiper@yandex.ru>
2020-06-30 20:26:12 +0300

    another one

--- fdf/srcs/fdf_map_parsing.c
+++ fdf/srcs/fdf_map_parsing.c
@@ -6,7 +6,7 @@
 /*   By: lcassaun <lcassaun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/25 17:23:10 by lcassaun          #+#    #+#             */
-/*   Updated: 2020/06/30 17:45:48 by lcassaun         ###   ########.fr       */
+/*   Updated: 2020/06/30 20:25:38 by lcassaun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,9 +63,9 @@ t_mlx	*map_parsing(char **av)
 		"./fdf <filename> [ case_size z_size ]\n"));
 	if (fdf_valid_malloc(mlx) == 0)
 		return (fdf_error(mlx, "Found wrong line length. Exiting.\n"));
-	if (!(mlx->pxl))
-		return (fdf_error(mlx, "No data found.\n"));
 	close(mlx->fd);
 	mlx->pxl = fdf_split(mlx, av);
+	if (!(mlx->pxl[0][0]))
+		return (fdf_error(mlx, "No data found.\n"));
 	return (mlx);
 }
