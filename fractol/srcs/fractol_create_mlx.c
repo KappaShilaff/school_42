6796d522b83c540f328203cab0b1e217bb842aca
KappaShilaff <ckiper@yandex.ru>
2020-07-04 20:44:36 +0300

    another one

--- fractol/srcs/fractol_create_mlx.c
+++ fractol/srcs/fractol_create_mlx.c
@@ -6,7 +6,7 @@
 /*   By: lcassaun <lcassaun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/04 18:57:48 by lcassaun          #+#    #+#             */
-/*   Updated: 2020/07/04 19:35:22 by lcassaun         ###   ########.fr       */
+/*   Updated: 2020/07/04 20:36:18 by lcassaun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,4 @@ void	fr_create_mlx(t_fr *fr)
 	fr->win = mlx_new_window(fr->mlx, WIDTH, HEIGHT, "Fractol");
 	fr->img = mlx_new_image(fr->mlx, WIDTH, HEIGHT);
 	fr->img_str = mlx_get_data_addr(fr->img, &fr->bpp, &fr->strlen, &fr->endian);
-	printf("%d\n", fr->bpp);
 }
