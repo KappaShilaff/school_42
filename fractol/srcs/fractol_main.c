cec0405b485903436f6823b1c835e55c6e57e9b4
KappaShilaff <ckiper@yandex.ru>
2020-07-04 20:18:04 +0300

    another one

--- fractol/srcs/fractol_main.c
+++ fractol/srcs/fractol_main.c
@@ -6,7 +6,7 @@
 /*   By: lcassaun <lcassaun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/04 18:24:09 by lcassaun          #+#    #+#             */
-/*   Updated: 2020/07/04 19:33:45 by lcassaun         ###   ########.fr       */
+/*   Updated: 2020/07/04 19:52:23 by lcassaun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ static int 	fr_valid_argv(t_fr *fr)
 	if (ft_strcmp(fr->argv, "Julia") == 0)
 		fr_julia(fr);
 	else if (ft_strcmp(fr->argv, "Mandelbrot") == 0)
-		fr_mandelbrot(fd);
+		fr_mandelbrot(fr);
 	else
 		return (0);
 	return (1);
