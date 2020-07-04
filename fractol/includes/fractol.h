3b6f9224c83dc1582af49a61d08df8f7a6e2a603
KappaShilaff <ckiper@yandex.ru>
2020-07-04 18:53:26 +0300

    main added

---
+++ fractol/includes/fractol.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fractol.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bzmuda <bzmuda@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/06/05 11:09:39 by bzmuda            #+#    #+#             */
+/*   Updated: 2020/07/04 18:30:11 by lcassaun         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FRACTOL_H
+# define FRACTOL_H
+
+# include <stdlib.h>
+# include <stdio.h>
+# include <math.h>
+# include "../srcs/libft/includes/libft.h"
+# include <fcntl.h>
+# include <mlx.h>
+
+typedef struct		s_fr
+{
+	char 			*argv;
+}					t_fr;
+
+void	fr_julia(t_fr *fr);
+
+#endif
---
+++ fractol/srcs/fractol_main.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fractol_main.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lcassaun <lcassaun@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/04 18:24:09 by lcassaun          #+#    #+#             */
+/*   Updated: 2020/07/04 18:49:23 by lcassaun         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/fractol.h"
+
+static int 	fr_error(char *str, int i_switch)
+{
+	ft_putstr_fd(str, 2);
+	if (i_switch == 1)
+	{
+		ft_putstr_fd("Valid number of arguments : 1\n", 2);
+	}
+	if (i_switch == 2)
+	{
+		ft_putstr_fd("Valid argument : Julia\n", 2);
+	}
+	return (0);
+}
+
+static int 	fr_valid_argv(t_fr *fr)
+{
+	if (ft_strcmp(fr->argv, "Julia") == 0)
+		fr_julia(fr);
+	else
+		return (0);
+}
+
+int		main(int argc, char **argv)
+{
+	t_fr	*fr;
+
+	if (argc != 2)
+		return (fr_error("Invalid number of arguments\n"), 1);
+	if (!(fr = malloc(sizeof(t_fr))))
+		return (fr_error("Struct malloc error\n", 3));
+	fr->argv = argv[1];
+	if (fr_valid_argv(fr) == 0)
+		return (fr_error("Invalid argument\n", 2));
+	return (0);
+}
