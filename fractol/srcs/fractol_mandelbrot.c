46f580b56c87e298b4e90d737825b7406137b777
KappaShilaff <ckiper@yandex.ru>
2020-07-06 22:22:01 +0300

    another one

--- fractol/srcs/fractol_mandelbrot.c
+++ fractol/srcs/fractol_mandelbrot.c
@@ -6,7 +6,7 @@
 /*   By: lcassaun <lcassaun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/04 19:50:19 by lcassaun          #+#    #+#             */
-/*   Updated: 2020/07/06 22:14:05 by lcassaun         ###   ########.fr       */
+/*   Updated: 2020/07/06 22:18:06 by lcassaun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,15 +22,15 @@ void 	fr_shift(t_fr *fr)
 
 static void	fr_fill_mandel(t_fr *fr, int i)
 {
-	fr[0].x = 0;
-	fr[0].y = 0;
-	fr[0].i = 0;
-	fr[0].x1 = -5.5;
-	fr[0].y1 = -3.25;
-	fr[0].itmax = 40;
-	fr[0].zoom = 150;
-	fr[0].fr_switch = 2;
-	fr[0].thread = i;
+	fr->x = 0;
+	fr->y = 0;
+	fr->i = 0;
+	fr->x1 = -5.5;
+	fr->y1 = -3.25;
+	fr->itmax = 40;
+	fr->zoom = 150;
+	fr->fr_switch = 2;
+	fr->thread = i;
 }
 
 static void	fr_calc_mandel(t_fr *fr)
@@ -80,25 +80,24 @@ void		fr_mandelbrot_draw(t_fr **fr)
 	mlx_loop(fr[0]->mlx);
 }
 
-void 		fr_fill_another(t_fr *fr)
+void 		fr_fill_another(t_fr **fr)
 {
-	fr[1].img_str = fr[0].img_str;
-	fr[1].win = fr[0].win;
-	fr[1].img = fr[0].img;
-	fr[1].mlx = fr[0].mlx;
-	fr[2].mlx = fr[0].mlx;
-	fr[2].win = fr[0].win;
-	fr[2].img = fr[0].img;
-	fr[2].img_str = fr[0].img_str;
+	fr[1]->img_str = fr[0]->img_str;
+	fr[1]->win = fr[0]->win;
+	fr[1]->img = fr[0]->img;
+	fr[1]->mlx = fr[0]->mlx;
+	fr[2]->mlx = fr[0]->mlx;
+	fr[2]->win = fr[0]->win;
+	fr[2]->img = fr[0]->img;
+	fr[2]->img_str = fr[0]->img_str;
 }
 
 void		fr_mandelbrot(t_fr **fr)
 {
 	fr_create_mlx(fr);
-	fr_fill_another(*fr);
+	fr_fill_another(fr);
 	fr_fill_mandel(fr[0], 0);
 	fr_fill_mandel(fr[1], 1);
 	fr_fill_mandel(fr[2], 2);
-	printf("kek\n");
 	fr_mandelbrot_draw(fr);
 }
