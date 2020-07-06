a4adb93ef81b529a44c11ad03dd143d1d570eaed
KappaShilaff <ckiper@yandex.ru>
2020-07-06 18:57:56 +0300

    another one

--- fractol/srcs/fractol_main.c
+++ fractol/srcs/fractol_main.c
@@ -6,7 +6,7 @@
 /*   By: lcassaun <lcassaun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/04 18:24:09 by lcassaun          #+#    #+#             */
-/*   Updated: 2020/07/06 18:46:51 by lcassaun         ###   ########.fr       */
+/*   Updated: 2020/07/06 18:55:16 by lcassaun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,12 +22,12 @@ static int 	fr_error(char *str, int i_switch)
 	return (0);
 }
 
-static int 	fr_valid_argv(t_fr *fr)
+static int 	fr_valid_argv(t_fr **fr)
 {
-	if (ft_strcmp(fr[0].argv, "Julia") == 0)
-		fr_julia(fr);
-	else if (ft_strcmp(fr[0].argv, "Mandelbrot") == 0)
-		fr_mandelbrot(&fr);
+	if (ft_strcmp(fr[0]->argv, "Julia") == 0)
+		fr_julia(*fr);
+	else if (ft_strcmp(fr[0]->argv, "Mandelbrot") == 0)
+		fr_mandelbrot(fr);
 	else
 		return (0);
 	return (1);
@@ -35,14 +35,15 @@ static int 	fr_valid_argv(t_fr *fr)
 
 int		main(int argc, char **argv)
 {
-	t_fr				fr[4];
+	t_fr					*fr;
 	pthread_t 		thread_id[4];
 
 	if (argc != 2)
 		return (fr_error("Invalid number of arguments\n", 1));
+	fr = malloc(sizeof(t_fr) * 4);
 	fr[0].argv = argv[1];
 	fr[0].thread_id = thread_id;
-	if (fr_valid_argv(fr) == 0)
+	if (fr_valid_argv(&fr) == 0)
 		return (fr_error("Invalid argument\n", 2));
 	return (0);
 }
