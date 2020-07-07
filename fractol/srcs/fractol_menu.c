c2e963ccfcd5524ee5f1c799155f7a5d63c886a2
KappaShilaff <ckiper@yandex.ru>
2020-07-08 00:27:50 +0300

    another one

--- fractol/srcs/fractol_menu.c
+++ fractol/srcs/fractol_menu.c
@@ -6,7 +6,7 @@
 /*   By: lcassaun <lcassaun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/07 23:24:29 by lcassaun          #+#    #+#             */
-/*   Updated: 2020/07/07 23:37:39 by lcassaun         ###   ########.fr       */
+/*   Updated: 2020/07/08 00:26:13 by lcassaun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,12 @@ void	fr_draw_menu(t_fr **fr)
 	if (fr[0]->menu == 340)
 	{
 		mlx_string_put(fr[0]->mlx, fr[0]->win,
-					   WIDTH - 330, 30, 0xffffff, "key_shift  - w a s d");
+					   WIDTH - 330, 30, 0xffffff, "Shift     - w a s d");
 		mlx_string_put(fr[0]->mlx, fr[0]->win,
-					   WIDTH - 330, 60, 0xffffff, "key_zoom   - mouse scroll");
+					   WIDTH - 330, 60, 0xffffff, "Zoom      - mouse scroll");
 		mlx_string_put(fr[0]->mlx, fr[0]->win,
-					   WIDTH - 330, 210, 0xffffff, "key_exit   - esc");
+					   WIDTH - 330, 90, 0xffffff, "Hide menu - mouse scroll");
+		mlx_string_put(fr[0]->mlx, fr[0]->win,
+					   WIDTH - 330, 210, 0xffffff, "Exit      - esc");
 	}
 }
