dec49ebab69f270eac120848a2320a3c19bbe5e2
KappaShilaff <ckiper@yandex.ru>
2020-07-07 23:37:09 +0300

    another one

--- fractol/srcs/fractol_create_mlx.c
+++ fractol/srcs/fractol_create_mlx.c
@@ -6,7 +6,7 @@
 /*   By: lcassaun <lcassaun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/04 18:57:48 by lcassaun          #+#    #+#             */
-/*   Updated: 2020/07/06 22:14:53 by lcassaun         ###   ########.fr       */
+/*   Updated: 2020/07/07 23:32:28 by lcassaun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void	fr_create_mlx(t_fr **fr)
 {
 	fr[0]->mlx = mlx_init();
-	fr[0]->win = mlx_new_window(fr[0]->mlx, WIDTH + 340, HEIGHT, "Fractol");
+	fr[0]->win = mlx_new_window(fr[0]->mlx, WIDTH, HEIGHT, "Fractol");
 	fr[0]->img = mlx_new_image(fr[0]->mlx, WIDTH, HEIGHT);
 	fr[0]->img_str = mlx_get_data_addr(fr[0]->img, &fr[0]->bpp, &fr[0]->strlen, &fr[0]->endian);
 	mlx_mouse_hook(fr[0]->win, fr_mouse, fr);
--- fractol/srcs/fractol_menu.c
+++ fractol/srcs/fractol_menu.c
@@ -6,7 +6,7 @@
 /*   By: lcassaun <lcassaun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/07 23:24:29 by lcassaun          #+#    #+#             */
-/*   Updated: 2020/07/07 23:27:15 by lcassaun         ###   ########.fr       */
+/*   Updated: 2020/07/07 23:36:50 by lcassaun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,10 @@
 
 void	fr_draw_menu(t_fr **fr)
 {
-	mlx_string_put(fr[0]->mlx, fr[0]->win, WIDTH - 330, 30, 0xffffff, "kekw");
+	mlx_string_put(fr[0]->mlx, fr[0]->mlx,
+				   WIDTH - 330, 30, 0xffffff, "key_shift  - w a s d");
+	mlx_string_put(fr[0]->mlx, fr[0]->mlx,
+				   WIDTH - 330, 60, 0xffffff, "key_zoom   - mouse scroll");
+	mlx_string_put(fr[0]->mlx, fr[0]->mlx,
+				   WIDTH - 330, 210, 0xffffff, "key_exit   - esc");
 }
