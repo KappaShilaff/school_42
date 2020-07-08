cafd7a538b51f0b88c9ad3a99763fb8fe94a5494
KappaShilaff <ckiper@yandex.ru>
2020-07-08 17:11:49 +0300

    another one

--- fractol/srcs/fractol_draw.c
+++ fractol/srcs/fractol_draw.c
@@ -6,7 +6,7 @@
 /*   By: lcassaun <lcassaun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/04 20:01:52 by lcassaun          #+#    #+#             */
-/*   Updated: 2020/07/08 17:09:29 by lcassaun         ###   ########.fr       */
+/*   Updated: 2020/07/08 17:10:00 by lcassaun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ void 	fr_color(t_fr *fr)
 //	fr->color_temp = 0x0000ff;
 //	fr->rgb1 = 0x010000;
 //	fr->rgb2 = 0x000100;
-	fr->color_temp = 0x0000ff + ((0x010000 * 255) / fr->itmax) * fr->i + ((0x000100 * 255) / fr->itmax) * fr->i;
+	fr->color_temp = 0x0000ff + 0x010000 * (255 * fr->i / fr->itmax) + 0x000100 * (255 * fr->i / fr->itmax);
 }
 
 void			fr_bfr_pix(t_fr *fr)
