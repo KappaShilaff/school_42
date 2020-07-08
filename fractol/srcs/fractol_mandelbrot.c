efd975ea9c3296c045c932e6a4a3dfc0a086f1d4
KappaShilaff <ckiper@yandex.ru>
2020-07-08 16:18:25 +0300

    another one

--- fractol/srcs/fractol_mandelbrot.c
+++ fractol/srcs/fractol_mandelbrot.c
@@ -6,7 +6,7 @@
 /*   By: lcassaun <lcassaun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/04 19:50:19 by lcassaun          #+#    #+#             */
-/*   Updated: 2020/07/08 16:17:46 by lcassaun         ###   ########.fr       */
+/*   Updated: 2020/07/08 16:18:05 by lcassaun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ static void	fr_fill_mandel(t_fr *fr, int i)
 	fr->zoom = 150;
 	fr->fr_switch = 2;
 	fr->thread = i;
-	fr->rgb = 35
+	fr->rgb = 35;
 }
 
 static void	fr_calc_mandel(t_fr *fr)
