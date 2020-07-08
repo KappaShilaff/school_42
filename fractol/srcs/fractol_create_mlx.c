57dc40e66cb4d20c9344fd8fd698b241f7df161d
KappaShilaff <ckiper@yandex.ru>
2020-07-08 18:33:45 +0300

    another one

--- fractol/srcs/fractol_create_mlx.c
+++ fractol/srcs/fractol_create_mlx.c
@@ -6,43 +6,22 @@
 /*   By: lcassaun <lcassaun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/04 18:57:48 by lcassaun          #+#    #+#             */
-/*   Updated: 2020/07/08 17:51:44 by lcassaun         ###   ########.fr       */
+/*   Updated: 2020/07/08 18:31:46 by lcassaun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fractol.h"
 
-void	fr_fill_another(t_fr **fr)
+void	fr_create_mlx(t_fr *fr)
 {
-	fr[1]->win = fr[0]->win;
-	fr[1]->img = fr[0]->img;
-	fr[1]->mlx = fr[0]->mlx;
-	fr[1]->img_str = fr[0]->img_str;
-	fr[1]->menu = fr[0]->menu;
-	fr[2]->mlx = fr[0]->mlx;
-	fr[2]->win = fr[0]->win;
-	fr[2]->img = fr[0]->img;
-	fr[2]->img_str = fr[0]->img_str;
-	fr[2]->menu = fr[0]->menu;
-	fr[3]->mlx = fr[0]->mlx;
-	fr[3]->win = fr[0]->win;
-	fr[3]->img = fr[0]->img;
-	fr[3]->img_str = fr[0]->img_str;
-	fr[3]->menu = fr[0]->menu;
-}
-
-void	fr_create_mlx(t_fr **fr)
-{
-	fr[0]->mlx = mlx_init();
-	fr[0]->win = mlx_new_window(fr[0]->mlx, WIDTH, HEIGHT, "Fractol");
-	fr[0]->img = mlx_new_image(fr[0]->mlx, WIDTH - fr[0]->menu, HEIGHT);
-	fr[0]->img_str = mlx_get_data_addr(fr[0]->img,
-			&fr[0]->bpp, &fr[0]->strlen, &fr[0]->endian);
-	mlx_mouse_hook(fr[0]->win, fr_mouse, fr);
-	mlx_key_hook(fr[0]->win, fr_key, fr);
-	mlx_hook(fr[0]->win, 6, 0, fr_hook_move_mouse, fr);
-	fr_draw_menu(fr);
-	fr[0]->shift_y = 0;
-	fr[0]->shift_x = 0;
-	fr_fill_another(fr);
+	fr->mlx = mlx_init();
+	fr->win = mlx_new_window(fr->mlx, WIDTH, HEIGHT, "Fractol");
+	fr->img = mlx_new_image(fr->mlx, WIDTH, HEIGHT);
+	fr->img_str = mlx_get_data_addr(fr->img, &fr->bpp, &fr->strlen, &fr->endian);
+	mlx_mouse_hook(fr->win, fr_mouse, fr);
+	mlx_key_hook(fr->win, fr_key, fr);
+	fr->shift_y = 0;
+	fr->shift_x = 0;
+	fr->shift_x_sum = 0;
+	fr->shift_y_sum = 0;
 }
--- fractol/srcs/fractol_hook.c
+++ fractol/srcs/fractol_hook.c
@@ -6,93 +6,63 @@
 /*   By: lcassaun <lcassaun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/05 16:25:47 by lcassaun          #+#    #+#             */
-/*   Updated: 2020/07/08 18:07:17 by lcassaun         ###   ########.fr       */
+/*   Updated: 2020/07/08 18:31:46 by lcassaun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fractol.h"
 
-static void		fr_key_shift(int key, t_fr *fr)
+static void	fr_key_shift(int key, t_fr *fr)
 {
 	if (key == KEY_W)
 	{
-		fr->shift_y = -100 / fr->zoom;
+		fr->shift_y = -fr->zoom;
 		fr->shift_x = 0;
 	}
 	if (key == KEY_S)
 	{
-		fr->shift_y = 100 / fr->zoom;
+		fr->shift_y = fr->zoom;
 		fr->shift_x = 0;
 	}
 	if (key == KEY_A)
 	{
-		fr->shift_x = -100 / fr->zoom;
+		fr->shift_x = -fr->zoom;
 		fr->shift_y = 0;
 	}
 	if (key == KEY_D)
 	{
-		fr->shift_x = 100 / fr->zoom;
+		fr->shift_x = fr->zoom;
 		fr->shift_y = 0;
 	}
-	fr->x1 += fr->shift_x;
-	fr->y1 += fr->shift_y;
+	fr_shift(fr);
 }
 
-int				fr_mouse(int code, int x, int y, t_fr **fr)
+int		fr_mouse(int code, int x, int y, t_fr *fr)
 {
 	if (code == 4 || code == 1)
 	{
-		fr_zoom(fr[0], x, y);
-		fr_zoom(fr[1], x, y);
-		fr_zoom(fr[2], x, y);
-		fr_zoom(fr[3], x, y);
+		fr_zoom(fr, x, y);
 		fr_draw(fr);
 	}
 	else if (code == 5 || code == 2)
 	{
-		fr_dezoom(fr[0], x, y);
-		fr_dezoom(fr[1], x, y);
-		fr_dezoom(fr[2], x, y);
-		fr_dezoom(fr[3], x, y);
+		fr_dezoom(fr, x, y);
 		fr_draw(fr);
 	}
 	return (0);
 }
 
-void			fr_key_switch(int key, t_fr **fr)
+int		fr_key(int key, t_fr *fr)
 {
-	if (key == KEY_1)
-		fr_julia(fr);
-	if (key == KEY_2)
-		fr_ship(fr);
-	if (key == KEY_3)
-		fr_chameleon(fr);
-	if (key == KEY_4)
-		fr_mandelbrot(fr);
-}
-
-int				fr_key(int key, t_fr **fr)
-{
-	fr_key_switch(key, fr);
-	fr_color_switch(key, fr);
 	if (key == KEY_ESC)
 	{
-		free(fr[0]);
-		free(fr[1]);
-		free(fr[2]);
-		free(fr[3]);
 		free(fr);
 		exit(EXIT_SUCCESS);
 	}
 	if ((key >= 0 && key <= 2) || key == 13)
 	{
-		fr_key_shift(key, fr[0]);
-		fr_key_shift(key, fr[1]);
-		fr_key_shift(key, fr[2]);
-		fr_key_shift(key, fr[3]);
+		fr_key_shift(key, fr);
 		fr_draw(fr);
 	}
-	fr_move_switch(key, fr);
-	fr_hidemenu(key, fr);
 	return (0);
 }
